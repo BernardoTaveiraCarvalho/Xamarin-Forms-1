da3f5ff2910037452fdbb2ba5cff8f8718b08070
BernardoTaveiraCarvalho <bernardo.taveira.carvalho@gmail.com>
2022-08-30 22:00:58 +0100

    first commit

---
+++ Xamarin Forms 1/Xamarin Forms 1/Login.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Forms_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+           
+        }
+        private void Button_Clicked(object sender,EventArgs e)
+        {
+            //Validar credenciais (dummy validation) 
+            if(utilizador.Text =="user" && palavrapasse.Text == "pass")
+            {
+                //Navegar para a MainPage
+                Navigation.PushAsync(new MainPage());
+            }
+            else
+            {
+                DisplayAlert("Error","Login wrong","close");
+            }
+        }
+    }
+}
